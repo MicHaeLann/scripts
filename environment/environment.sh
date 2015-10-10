fdf50ad842f3c1ed4cb2955afb3e5d053b4a2c27
andanshi <an.danshi@taosecret.com>
2015-10-10 17:33:17 +0800

    remove source file

--- environment/environment.sh
+++ environment/environment.sh
@@ -19,22 +19,9 @@ MYSQL_FILE_NAME="$MYSQL_VERSION.tar.gz"
 MYSQL_PREFIX="$DATA_ROOT/mysql"
 
 # php config
-FREETYPE_VERSION="freetype-2.6"
-FREETYPE_FILE_NAME="$FREETYPE_VERSION.tar.gz"
-FREETYPE_PREFIX="$DATA_ROOT/freetype"
-JPEG_VERSION="jpeg-9a"
-JPEG_FILE_NAME="$JPEG_VERSION.tar.gz"
-JPEG_PREFIX="$DATA_ROOT/jpeg"
-LIBEVENT_VERSION="libevent-2.0.21-stable"
-LIBEVENT_FILE_NAME="$LIBEVENT_VERSION.tar.gz"
-LIBEVENT_PREFIX="$DATA_ROOT/libevent"
-LIBPNG_VERSION="libpng-1.6.17"
-LIBPNG_FILE_NAME="$LIBPNG_VERSION.tar.gz"
-LIBPNG_PREFIX="$DATA_ROOT/libpng"
-MEMCACHED_VERSION="memcached-1.4.24"
-MEMCACHED_FILE_NAME="$MEMCACHED_VERSION.tar.gz"
-MHASH_VERSION="mhash-0.9.9.9"
-MHASH_FILE_NAME="$MHASH_VERSION.tar.gz"
+MEMCACHE_VERSION="memcache-2.2.7"
+MEMCACHE_FILE_NAME="$MEMCACHE_VERSION.tgz"
+PHP_MIRROR="http://cn2.php.net/"
 PHP_VERSION="php-5.6.3"
 PHP_FILE_NAME="$PHP_VERSION.tar.gz"
 PHP_PREFIX="$DATA_ROOT/php"
@@ -53,20 +40,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # install indepence
-sudo apt-get install unzip
-sudo apt-get install libpcre3 libpcre3-dev libssl-dev openssl libgd2-xpm-dev
-sudo apt-get install build-essential  
-sudo apt-get install libncurses5-dev  
-sudo apt-get install sysv-rc-conf  
-sudo apt-get install cmake 
-sudo apt-get install libxml2-dev
-sudo apt-get install libcurl4-openssl-dev
-sudo apt-get install libmcrypt4
-sudo apt-get install libmcrypt-dev
-
-# unzip lnmp source file
-cd $DATA_SOURCE_FILE_ROOT
-sudo unzip $DATA_SOURCE_FILE_NAME
+sudo apt-get install unzip libpcre3 libpcre3-dev libssl-dev openssl libgd2-xpm-dev build-essential libncurses5-dev sysv-rc-conf cmake libxml2-dev libcurl4-openssl-dev libmcrypt4 libmcrypt-dev memcached libevent-dev autoconf
 
 # add user
 sudo groupadd www
@@ -76,6 +50,7 @@ sudo useradd -r -g mysql mysql
 
 # install nginx
 cd  $DATA_SOURCE_FILE_ROOT
+wget http://nginx.org/download/$NGINX_FILE_NAME
 tar -xzvf $NGINX_FILE_NAME
 cd $NGINX_VERSION
 ./configure --prefix="$NGINX_PREFIX" --with-http_ssl_module --with-http_image_filter_module --with-http_stub_status_module
@@ -95,6 +70,8 @@ cp -r "$DATA_SOURCE_FILE_ROOT/conf.d" "$NGINX_PREFIX/conf/conf.d"
 
 # install mysql
 cd $DATA_SOURCE_FILE_ROOT
+VERSION_PREFIX=${MYSQL_VERSION:6:3}
+wget http://dev.mysql.com/get/Downloads/MySQL-$VERSION_PREFIX/$MYSQL_FILE_NAME
 tar -xzvf $MYSQL_FILE_NAME
 cd $MYSQL_VERSION
 cmake -DCMAKE_INSTALL_PREFIX="$MYSQL_PREFIX" -DMYSQL_DATADIR="$MYSQL_PREFIX/data" -DWITH_MYISAM_STORAGE_ENGINE=1 -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWTIH_ARCHIVE_STORAGE_ENGINE=1 -DWITH_BLACKHOLE_STORAGE_ENGINE=1 -DENABLED_LOCAL_INFILE=1 -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci -DEXTRA_CHARSETS=all -DMYSQL_TCP_PORT=3306
@@ -121,19 +98,33 @@ ln -s $MYSQL_PREFIX/bin/mysqladmin mysqladmin
 
 # change ld.so.conf
 echo "$MYSQL_PREFIX/lib" >> /etc/ld.so.conf
+ldconfig
 
 # start mysql
 /etc/init.d/mysqld start
-
+exit
 # install php
-cd $DATA_SOURCE_FILE_ROOT
+wget wget -c $PHP_MIRROR/distributions/$PHP_FILE_NAME
 tar -xzvf $PHP_FILE_NAME
 cd $PHP_VERSION
 ./configure --prefix="$PHP_PREFIX" --with-config-file-path="$PHP_PREFIX" --enable-fpm --with-fpm-user=www --with-fpm-group=www --with-mysql="$MYSQL_PREFIX" --with-mysqli="$MYSQL_PREFIX/bin/mysql_config" --with-pdo-mysql="$MYSQL_PREFIX" --with-iconv-dir --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib --with-libxml-dir= --enable-xml --disable-rpath --enable-magic-quotes --enable-safe-mode --enable-bcmath --enable-shmop --enable-sysvsem --enable-inline-optimization --with-curl --enable-mbregex --enable-mbstring --with-mcrypt --enable-ftp --with-gd --enable-gd-native-ttf --with-openssl --with-mhash --enable-pcntl --enable-sockets --with-xmlrpc --enable-zip --enable-soap --without-pear --with-gettext
 sudo make && make install 
 
-# copy php-fpm config
+# copy php-fpm config & php.ini
 cp "$PHP_PREFIX/etc/php-fpm.conf.default" "$PHP_PREFIX/etc/php-fpm.conf"
+cp $DATA_SOURCE_FILE_ROOT/$PHP_VERSION/php.ini-production $PHP_PREFIX/lib/php.ini
+
+# start memcached server
+memcached -d -m 256 -p 11211 -u root -c 1024
+
+# install php_memcache
+#cd $DATA_SOURCE_FILE_ROOT
+#wget http://pecl.php.net/get/$MEMCACHE_FILE_NAME
+#tar -xzvf $MEMCACHE_FILE_NAME
+#cd $MEMCACHE_VERSION
+#$PHP_PREFIX/bin/phpize
+#./configure --enable-memcache --with-php-config="$DATA_PHP_PREFIX/bin/php-config" --with-zlib-dir
+#sudo make && make install
 
 # start nginx & php-fpm
 cd ~
